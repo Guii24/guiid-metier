55cb31d10e51816be6f9691280503c06070d8b9f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-08-11 14:38:09 +0000

    Updating to latest FlutterFlow output.

--- lib/nav_bar/user_bottom_nav_bar_copy/user_bottom_nav_bar_copy_model.dart
+++ lib/nav_bar/user_bottom_nav_bar_copy/user_bottom_nav_bar_copy_model.dart
@@ -1,11 +1,11 @@
-import '/flutter_flow/flutter_flow_icon_button.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class NavBar1Model extends FlutterFlowModel {
+class UserBottomNavBarCopyModel extends FlutterFlowModel {
   /// Initialization and disposal methods.
 
   void initState(BuildContext context) {}
